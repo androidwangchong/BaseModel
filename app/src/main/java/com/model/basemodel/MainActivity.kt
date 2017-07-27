c57c216fcf656cee8d4fa33457567fff7300d2d7
ozintel <zhuang.wang@ozstrategy.com>
2017-07-27 17:26:36 +0800

    基础框架

--- app/src/main/java/com/model/basemodel/MainActivity.kt
+++ app/src/main/java/com/model/basemodel/MainActivity.kt
@@ -1,70 +1,81 @@
 package com.model.basemodel
 
+import android.app.FragmentTransaction
 import android.os.Bundle
-import android.support.design.widget.TabLayout
-import android.support.v4.app.Fragment
-import android.support.v4.app.FragmentActivity
-import android.view.View
+import android.support.v7.app.AppCompatActivity
 import android.widget.FrameLayout
-import android.widget.TextView
-import com.model.basemodel.http.RetrofitClient
-import com.model.basemodel.http.api.DemoAPI
-import com.model.basemodel.http.api.model
-import com.model.basemodel.ui.adapter.HomePageFragmentAdapter
-import org.androidannotations.annotations.res.StringArrayRes
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import com.model.basemodel.ui.activity.credit.CreditFragment
+import com.model.basemodel.ui.activity.creditService.CreditServiceFragment
+import com.model.basemodel.ui.activity.mine.MineFragment
 import org.jetbrains.anko.find
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
-class MainActivity : FragmentActivity() {
-    lateinit var tl_container: TabLayout
-    lateinit var fl_container : FrameLayout
-    lateinit var pageAdapter : HomePageFragmentAdapter
-    val tabNames: Array<String> = arrayOf("信用","服务","我的")
-    val tabIcons: IntArray = intArrayOf(R.drawable.selector_credit,R.drawable.selector_credit,R.drawable.selector_credit)
+class MainActivity : AppCompatActivity(), RadioGroup.OnCheckedChangeListener {
+
+
+    lateinit var rd_group: RadioGroup
+    lateinit var fl_container: FrameLayout
+    lateinit var rd_menu_credit: RadioButton
+    lateinit var rd_menu_service: RadioButton
+    lateinit var rd_menu_mine: RadioButton
+     var creditFragment: CreditFragment? = null
+     var serviceFragment: CreditServiceFragment? = null
+     var mineFragment: MineFragment? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        tl_container = find(R.id.tl_container)
+        rd_group = find(R.id.rd_group)
         fl_container = find(R.id.fl_container)
-        pageAdapter = HomePageFragmentAdapter(supportFragmentManager)
+        rd_menu_credit = find(R.id.rd_menu_credit)
+        rd_menu_service = find(R.id.rd_menu_service)
+        rd_menu_mine = find(R.id.rd_menu_mine)
+        rd_group.setOnCheckedChangeListener(this)
+        rd_menu_credit.isChecked = true
+    }
 
-        for ( i in 0..tabNames.size) {
-            val tabView = View.inflate(this, R.layout.tab_indicator, null)
-             val textView = tabView.find<TextView>(R.id.tab_title)
-            textView.text =(tabNames[i])
-            // 利用这种办法设置图标是为了解决默认设置图标和文字出现的距离较大问题
-            textView.setCompoundDrawablesWithIntrinsicBounds(0, tabIcons[i], 0, 0);
-            tl_container.addTab(tl_container.newTab().setCustomView(textView));
+    fun hideAllFragment(transaction: FragmentTransaction) {
+        if (creditFragment != null) {
+            transaction.hide(creditFragment)
         }
-        // 初始化默认显示的fragment
-        val fragment =  pageAdapter.instantiateItem(fl_container, 0) as Fragment
-        pageAdapter.setPrimaryItem(fl_container, 0, fragment)
-        pageAdapter.finishUpdate(fl_container)
-        pageAdapter.destroyItem(fl_container, 0, fragment)
+        if (serviceFragment != null) {
+            transaction.hide(serviceFragment)
+        }
+        if (mineFragment != null) {
+            transaction.hide(mineFragment);
+        }
+    }
 
-        // Tablayout选择tab监听
-        tl_container.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
-            override fun onTabReselected(tab: TabLayout.Tab?) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onCheckedChanged(group: RadioGroup?, checkedId: Int) {
+        val transaction: FragmentTransaction = getFragmentManager().beginTransaction()
+        hideAllFragment(transaction)
+        when (checkedId) {
+            R.id.rd_menu_credit -> {
+                if (creditFragment == null) {
+                    creditFragment = CreditFragment()
+                    transaction.add(R.id.fl_container, creditFragment)
+                } else {
+                    transaction.show(creditFragment)
+                }
             }
-
-            override fun onTabUnselected(tab: TabLayout.Tab?) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            R.id.rd_menu_service -> {
+                if (serviceFragment == null) {
+                    serviceFragment = CreditServiceFragment()
+                    transaction.add(R.id.fl_container, serviceFragment)
+                } else {
+                    transaction.show(serviceFragment)
+                }
             }
-
-            override fun onTabSelected(tab: TabLayout.Tab?) {
-                val position = tl_container.getSelectedTabPosition()
-                val fragment = pageAdapter.instantiateItem(tl_container, position)
-                pageAdapter.setPrimaryItem(tl_container, position, fragment)
-                pageAdapter.finishUpdate(tl_container)
-                pageAdapter.destroyItem(tl_container, position, fragment)
+            R.id.rd_menu_mine -> {
+                if (mineFragment == null) {
+                    mineFragment = MineFragment()
+                    transaction.add(R.id.fl_container, mineFragment)
+                } else {
+                    transaction.show(mineFragment)
+                }
             }
-
-        })
-
-
+        }
+        transaction.commit()
     }
 }
