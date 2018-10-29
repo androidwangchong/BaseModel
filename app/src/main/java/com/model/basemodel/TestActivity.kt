0be83ab0632cc4ccc0bf02e9315d52b36a32330f
ozintel <zhuang.wang@ozstrategy.com>
2018-10-29 15:24:26 +0800

    fix the bug

--- app/src/main/java/com/model/basemodel/TestActivity.kt
+++ app/src/main/java/com/model/basemodel/TestActivity.kt
@@ -1,9 +1,6 @@
 package com.model.basemodel
 
 import com.model.basemodel.ui.activity.base.BaseActivity
-import kotlinx.android.synthetic.main.activity_main.*
-import org.jetbrains.anko.sdk25.coroutines.onClick
-import com.model.basemodel.util.openLoginActivity
 
 
 /**
@@ -11,13 +8,15 @@ import com.model.basemodel.util.openLoginActivity
  * Created by wangchong on 2017/7/27.
  */
 class TestActivity : BaseActivity() {
+    override fun getIntentMessageData() {
+
+    }
+
     override val title: String = "测试activity"
     override val layoutResId: Int = R.layout.activity_main
 
     override fun initView() {
-        text.onClick {
-            openLoginActivity()
-        }
+
 
 
     }
